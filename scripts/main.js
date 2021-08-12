8059236bcdfb5217c53bd2788f28e02328481096
JJ1GUJ <jj1guj@jarl.com>
2021-08-12 20:26:16 +0900

    盤面クラス・合法手リストを作成

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,161 @@
+//1を黒石, 2を白石として処理を行う
+class Board{
+    constructor(){
+        this.board=[];
+        this.turn=false;//trueなら後手
+        this.point;
+        this.stone= [1,2];
+        this.flip_limit;
+        this.di=[0,0,-1,1,-1,1,-1,1];
+        this.dj=[-1,1,0,0,1,1,-1,-1];
+    }
+
+    init(){
+        for(var i=0;i<8;++i){
+            let board_line=[];
+            for(var j=0;j<8;++j)board_line.push(0);
+            this.board.push(board_line);
+        }
+        this.board[3][3]=2;
+        this.board[3][4]=1;
+        this.board[4][3]=1;
+        this.board[4][4]=2;
+        this.point=[2,2];
+    }
+
+    is_gameover(){
+    }
+
+    push(move){
+        if(0<=move&&move<=63){
+            row=Math.floor(move/8);
+            col=move%8;
+            if(this.board[row][col]!=0)return -1;
+            
+            fliped=set_flip_limit(row,col);
+            if(fliped==0)return -1;
+
+            //石を返す
+            this.board[row][col]=this.stone[this.turn];
+
+            for(var dir=0;dir<8;++dir){
+                for(var i=1;i<this.flip_limit[dir];++i){
+                    this.board[row+this.di[dir]*i][col+this.dj[dir]*i]=this.stone[turn];
+                }
+            }
+
+            //着手後の石の枚数を計算
+            this.point[turn]+=fliped+1;
+            this.point[!turn]-=fliped;
+
+            this.turn=!this.turn;//手番を反転
+            return 0;
+        }
+    }
+
+    set_flip_limit(row,col){
+        //返せる石の枚数が返り値
+        var flip_count=0;
+        //横左方向
+        this.flip_limit=[0];
+        for(var i=1;i<=col;++i){
+            if(this.board[row][col-i]!=this.stone[!turn]){
+                if(board[row][col-i]==this.stone[turn])flip_limit[0]=i;
+                break;
+            }
+        }
+        if(flip_limit[0]>1)flip_count+=flip_limit[0]-1;
+
+        //横右方向
+        this.flip_limit.push(0);
+        for(var i=1;i<=7-col;++i){
+            if(this.board[row][col+i]!=this.stone[!turn]){
+                if(board[row][col+i]==this.stone[turn])flip_limit[1]=i;
+                break;
+            }
+        }
+        if(flip_limit[1]>1)flip_count+=flip_limit[1]-1;
+
+        //縦上方向
+        this.flip_limit.push(0);
+        for(var i=1;i<=row;++i){
+            if(this.board[row-i][col]!=this.stone[!turn]){
+                if(board[row-i][col]==this.stone[turn])flip_limit[2]=i;
+                break;
+            }
+        }
+        if(flip_limit[2]>1)flip_count+=flip_limit[2]-1;
+
+        //縦下方向
+        this.flip_limit.push(0);
+        for(var i=1;i<=7-row;++i){
+            if(this.board[row+i][col]!=this.stone[!turn]){
+                if(board[row+i][col]==this.stone[turn])flip_limit[3]=i;
+                break;
+            }
+        }
+        if(flip_limit[3]>1)flip_count+=flip_limit[3]-1;
+
+        //右斜め上方向
+        this.flip_limit.push(0);
+        for(var i=1;i<=Math.min(row,7-col);++i){
+            if(this.board[row-i][col+i]!=this.stone[!turn]){
+                if(board[row-i][col+i]==this.stone[turn])flip_limit[4]=i;
+                break;
+            }
+        }
+        if(flip_limit[4]>1)flip_count+=flip_limit[4]-1;
+
+        //右斜め下方向
+        this.flip_limit.push(0);
+        for(var i=1;i<=Math.min(7-row,7-col);++i){
+            if(this.board[row+i][col+i]!=this.stone[!turn]){
+                if(board[row+i][col+i]==this.stone[turn])flip_limit[5]=i;
+                break;
+            }
+        }
+        if(flip_limit[5]>1)flip_count+=flip_limit[5]-1;
+
+        //左斜め上方向
+        this.flip_limit.push(0);
+        for(var i=1;i<=Math.min(row,col);++i){
+            if(this.board[row-i][col-i]!=this.stone[!turn]){
+                if(board[row-i][col-i]==this.stone[turn])flip_limit[6]=i;
+                break;
+            }
+        }
+        if(flip_limit[6]>1)flip_count+=flip_limit[6]-1;
+
+        //左斜め下方向
+        this.flip_limit.push(0);
+        for(var i=1;i<=Math.min(7-row,col);++i){
+            if(this.board[row+i][col-i]!=this.stone[!turn]){
+                if(board[row+i][col-i]==this.stone[turn])flip_limit[7]=i;
+                break;
+            }
+        }
+        if(flip_limit[7]>1)flip_count+=flip_limit[7]-1;
+        return flip_limit;
+    }
+}
+
+//合法手のリストを生成
+function LegalMoveList(board){
+    let move_num=0;
+    let movelist=[];
+    for(var i=0;i<8;++i)for(var j=0;j<8;++j){
+        if(board.board[i][j]==0){
+            board.set_flip_limit(i,j);
+            for(var k=0;k<8;++k){
+                if(board.flip_limit[k]>1){
+                    movelist.push(8*i+j);
+                    break;
+                }
+            }
+        }
+    }
+}
+
 function makeMove(){
     id=Number(this.getAttribute("id"));
     console.log(id);

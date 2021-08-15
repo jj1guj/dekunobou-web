04b2796ac45bdef7c2b967f77cf3f4e0ff9daf68
JJ1GUJ <jj1guj@jarl.com>
2021-08-15 15:38:02 +0900

    エンジン側の指し手を返すようにした

--- api/api.py
+++ api/api.py
@@ -1,11 +1,16 @@
 from flask import Flask,make_response,request
 from flask_cors import CORS
+import subprocess
 api=Flask(__name__)
+CORS(api)
 
 #エンジンからの指し手を送る
 @api.route("/put",methods=["PUT"])
 def put():
-    return 0
+    board=request.form["board"]
+    turn=request.form["turn"]
+    cp=subprocess.run(["dekunobou",board,turn],encoding="utf-8",stdout=subprocess.PIPE)
+    return str(int(cp.stdout))
 
 #人間からの指し手を受け取る
 @api.route("/send",methods=["SEND"])

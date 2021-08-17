dd15ab024510c06b3fee0e4c44ef1709da56db49
JJ1GUJ <jj1guj@jarl.com>
2021-08-17 11:05:32 +0900

    herokuデプロイ用に準備

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask,make_response,request
 from flask_cors import CORS
 import subprocess
+import os
 api=Flask(__name__)
 CORS(api)
 
@@ -12,11 +13,15 @@ def put():
     cp=subprocess.run(["dekunobou",board,turn],encoding="utf-8",stdout=subprocess.PIPE)
     return str(int(cp.stdout))
 
-#人間からの指し手を受け取る
+#人間からの指し手を受け取る(いらない)
 @api.route("/send",methods=["SEND"])
 def send():
     move=request.form["move"]
     return make_response(move)
 
+@api.route("/")
+def index():
+    return "I'm dekunobou, computer othello program!"
+
 if __name__ == "__main__":
-    api.run(debug=True)
+    api.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
